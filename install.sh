9f2dc121e10b12dafa9adca91bb9a69129e9049a
jiekliao <jiekliao1314@163.com>
2015-11-15 02:46:36 -0500

    modify install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,8 @@ easy_install -U pep8 autopep8 >/dev/null
 
 if [ $? -eq 0 ];then
     echo "Start install .vim and .vimrc!"
-    rm -rf ~/.vim  ~/.vimrc
-    cp -r vim ~/.vim
-    cp vimrc ~/.vimrc
+    mv ~/.vim ~/.vim.bak && mv ~/.vimrc ~/.vimrc.bak
+    cp -r vim ~/.vim && cp vimrc ~/.vimrc
     echo "Vim is set successfully!"
 else
     echo "Error in installing pep8 and autopep8!"
