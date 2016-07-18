5dc7eac273972010e04d0cdb6aface638bb54fe1
root <root@localhost>
2016-07-18 11:53:21 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,13 @@ echo "Start installing .vim and .vimrc!"
 
 rm -rf ~/.vim.bak ~/.vimrc.bak >/dev/null
 
-mv ~/.vim ~/.vim.bak && mv ~/.vimrc ~/.vimrc.bak
+mv ~/.vim ~/.vim.bak >/dev/null
+mv ~/.vimrc ~/.vimrc.bak >/dev/null
 
 cp -r vim ~/.vim && cp vimrc ~/.vimrc
-
-echo "Vim is set up successfully!"
+if [ $? -eq 0 ];then
+    echo "Vim is set up successfully!"
+else
+    echo "Error!"
 
 
