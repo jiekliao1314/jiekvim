396ba16ef05e1cb750b8c9663331cc253bc82df7
jiekliao <jiekliao1314@163.com>
2016-07-19 14:49:35 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,13 @@ rm -rf ~/.vim.bak ~/.vimrc.bak >/dev/null
 mv ~/.vim ~/.vim.bak >/dev/null
 mv ~/.vimrc ~/.vimrc.bak >/dev/null
 
-cp -r vim ~/.vim && cp vimrc ~/.vimrc
+cp vimrc ~/.vimrc && cp -r vim ~/.vim 
 if [ $? -eq 0 ];then
     echo "Vim is set up successfully!"
+    echo "--------------------------"
+    echo "please make sure blew have been installed:"
+    echo "*ctags"
+    echo "--------------------------"
 else
-    echo "Error!"
+    echo "Failed in setting up vim!"
 fi
