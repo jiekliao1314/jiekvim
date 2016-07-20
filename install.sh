6b66ca5e2b903744ac8118e0630615a637da1d23
jiekliao <jiekliao1314@163.com>
2016-07-20 09:27:32 +0800

    modify install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-echo "Start installing .vim and .vimrc!"
+echo "Start setting up vim..." 
 
 rm -rf ~/.vim.bak ~/.vimrc.bak >/dev/null
 
@@ -8,10 +8,11 @@ mv ~/.vim ~/.vim.bak >/dev/null
 mv ~/.vimrc ~/.vimrc.bak >/dev/null
 
 cp vimrc ~/.vimrc && cp -r vim ~/.vim 
+
 if [ $? -eq 0 ];then
     echo "Vim is set up successfully!"
     echo "--------------------------"
-    echo "please make sure blew have been installed:"
+    echo "Make sure the following packages installed:"
     echo "*ctags"
     echo "--------------------------"
 else
